66e03cae605402ee5399ddebd069c62f035dbe80
sakamuuy <sakamuu.y@gmail.com>
2022-01-20 23:08:52 +0900

    Add condition

--- hello.rb
+++ hello.rb
@@ -1,1 +1,11 @@
-puts 'Hello'
+def hello(names)
+  names.each do |name|
+    if (name == 'MRI')
+      puts "HELLO, #{name.upcase}"
+    end
+  end
+end
+
+rubies = ['MRI', 'jruby', 'rubinius']
+
+hello(rubies)
